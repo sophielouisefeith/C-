715bbcaf45170cce2b9dfc15cfa3bfa261ab5a36
Sophie Feith <sfeith@f0r1s8.codam.nl>
2021-03-02 14:49:45 +0100

    merging

--- module07/ex02/Array.hpp
+++ module07/ex02/Array.hpp
@@ -6,17 +6,15 @@
 /*   By: SophieLouiseFeith <SophieLouiseFeith@st      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/02/26 15:23:40 by sfeith        #+#    #+#                 */
-/*   Updated: 2021/03/02 12:57:24 by SophieLouis   ########   odam.nl         */
+/*   Updated: 2021/03/02 13:53:04 by sfeith        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
-#include <exception>
-#include <iostream>
+#include "Color.hpp"
 
-
-/*  
+/* 
 • Construction with no parameter: creates an empty array.
 • Construction with an unsigned int n as a parameter: creates an array of n elements, initialized by default. (Tip: try to compile int * a = new int();, then
 display *a.)
@@ -31,34 +29,18 @@ a std::exception is thrown.
 member function takes no parameter and does not modify the current instance in
 any way */
 
-template <class T>
-
-class Array{
-        typedef unsigned int size;
-
-
-    public:
-
-    Array();
-    Array(size s);
-    Array(const Array &array);
-    /* deconstructor we delete  */
-    ~Array() {
-			delete[] _storage;
-		}
-    
-    
-}
-
-#ifndef ARRAY_H	
-#define ARRAY_H
 
 #include <iostream>
 
 template <typename T>
 
 class Array{
+
+    private:
+		unsigned int _size;
+		T *_array;
 	public:
+    /* empty array */
 		Array<T>() : _size(0), _array(NULL) {}
 		Array<T>(unsigned int n) : _size(n){
 		_array = new T[n];
@@ -66,11 +48,15 @@ class Array{
 		~Array<T>(){
 			delete [] this->_array;
 		}
+
 		Array<T>(const Array<T> &copy){
 			for (int i = 0; i < this->_size; i++)
 				this->_array[i] = copy._array[i];
 			this->_size = copy._size;
 		}
+
+
+        
 		Array<T> &operator=(const Array<T> &current){
 			if (this == &current)
 				return *this;
@@ -81,25 +67,26 @@ class Array{
 			return *this;
 		}
 		
-		unsigned int size(void) const {return this->_size;}
+        
+		unsigned int size(void) const 
+        {return this->_size;}
 		
 		T& operator[](unsigned int i){
 			if (!this->_array || i >= this->_size || i < 0)
-				throw Array<T>::OutOfBounds();
+				throw Array<T>::NotCorrect();
 			return this->_array[i];
 		}
 		
-		class OutOfBounds : public std::exception{
+
+        /* error class  */
+		class NotCorrect: public std::exception{
 			public:
-				OutOfBounds() throw() {}
-				~OutOfBounds() throw() {}
-				OutOfBounds(const OutOfBounds&) throw() {}
-				OutOfBounds &operator=(const OutOfBounds&) throw() {}
-				const char *what() const throw() {return "Input is out of bounds.";}
+				NotCorrect() throw() {}
+				~NotCorrect() throw() {}
+				NotCorrect(const NotCorrect&) throw() {}
+				NotCorrect&operator=(const NotCorrect&) throw() {}
+				const char *what() const throw() {return "the input is not correct";}
 		} ;
-	private:
-		unsigned int _size;
-		T *_array;
+	
 } ;
 
-#endif
--- module07/ex02/main.cpp
+++ module07/ex02/main.cpp
@@ -6,57 +6,58 @@
 /*   By: SophieLouiseFeith <SophieLouiseFeith@st      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/02/26 15:23:44 by sfeith        #+#    #+#                 */
-<<<<<<< HEAD
-/*   Updated: 2021/03/02 12:58:02 by SophieLouis   ########   odam.nl         */
-=======
-/*   Updated: 2021/03/02 12:55:37 by sfeith        ########   odam.nl         */
->>>>>>> c474b0abfc217205b83c6c9ecd2789ae1131ad2f
+/*   Updated: 2021/03/02 13:54:02 by sfeith        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Array.hpp"
 
 
-
-int main(void){
-
-
-
-    return 0;
-}
-
-
-#include "Array.hpp"
-
 int main(){
 	Array<int> test1(3);
-	Array<float> test2(5);
-	Array<char> test3(7);
+	std::cout << YELLOW << " INT "<< RESET << std::endl ;
+	
 	try {
+		/* construnction creates an array  */
 		for (unsigned int i = 0; i < test1.size(); i++)
 			std::cout << test1[i] << " ";
 		std::cout << std::endl ;
-		std::cout << "-- Trying to access an element that is out of bounds. --" << std::endl ;
-		test1[5];
+		
 	}
 	catch (std::exception &e){
 		std::cout << e.what() << std::endl ;
 	}
+
+	std::cout << YELLOW << " FLOAT "<< RESET << std::endl ;
+	
+	Array<float> test2(4);
 	try{
+		/* construnction creates an array  */
 		for (unsigned int i = 0; i < test2.size(); i++){
-			test2[i] = i + 0.1 ;
+			test2[i] = i + 65.46 ;
 			std::cout << test2[i] << " ";
+			std::cout << YELLOW << " display  the number of elements in the array : "<<test2.size() << RESET << std::endl ;
 		}
-		std::cout << std::endl ;
+	}
+		
+	catch (std::exception &e){
+		std::cout << e.what() << std::endl ;
+	}
+
+	std::cout << YELLOW << " CHARACTER "<< RESET << std::endl ;
+	Array<char> test3(8);
+	try{
 		for (unsigned int i = 0; i < test3.size(); i++){
-			test3[i] = 97 + i;
-			std::cout << test3[i] << " ";
+			test3[i] = 100 + i;
+			std::cout << "Im a character :" <<std::endl;
+			std::cout << test3[i] << " "<< "how many characters"<<i <<std::endl;
 		}
 		std::cout << std::endl ; 
-		std::cout << test3.size() << std::endl ;
+		std::cout << YELLOW << " display  the number of elements in the array : "<<test3.size() << RESET << std::endl ;
 	}
 	catch(std::exception &e){
 		std::cout << e.what() << std::endl;
 	}
 	return (0);
+	
 }
