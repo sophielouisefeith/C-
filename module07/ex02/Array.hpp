35561bac404f31ac3cc0625429c187f065f05822
Sophie Louise Feith <sfeith@student.codam.nl>
2021-03-02 12:58:16 +0100

    merging

--- module07/ex02/Array.hpp
+++ module07/ex02/Array.hpp
@@ -3,10 +3,10 @@
 /*                                                        ::::::::            */
 /*   Array.hpp                                          :+:    :+:            */
 /*                                                     +:+                    */
-/*   By: sfeith <sfeith@student.codam.nl>             +#+                     */
+/*   By: SophieLouiseFeith <SophieLouiseFeith@st      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/02/26 15:23:40 by sfeith        #+#    #+#                 */
-/*   Updated: 2021/02/26 15:29:54 by sfeith        ########   odam.nl         */
+/*   Updated: 2021/03/02 12:57:24 by SophieLouis   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,3 +49,57 @@ class Array{
     
     
 }
+
+#ifndef ARRAY_H	
+#define ARRAY_H
+
+#include <iostream>
+
+template <typename T>
+
+class Array{
+	public:
+		Array<T>() : _size(0), _array(NULL) {}
+		Array<T>(unsigned int n) : _size(n){
+		_array = new T[n];
+		}
+		~Array<T>(){
+			delete [] this->_array;
+		}
+		Array<T>(const Array<T> &copy){
+			for (int i = 0; i < this->_size; i++)
+				this->_array[i] = copy._array[i];
+			this->_size = copy._size;
+		}
+		Array<T> &operator=(const Array<T> &current){
+			if (this == &current)
+				return *this;
+			delete [] this->_array;
+			this->_size = current._size;
+			for (int i = 0; i < current._size;)
+				this->_array[i] = current._array[i];
+			return *this;
+		}
+		
+		unsigned int size(void) const {return this->_size;}
+		
+		T& operator[](unsigned int i){
+			if (!this->_array || i >= this->_size || i < 0)
+				throw Array<T>::OutOfBounds();
+			return this->_array[i];
+		}
+		
+		class OutOfBounds : public std::exception{
+			public:
+				OutOfBounds() throw() {}
+				~OutOfBounds() throw() {}
+				OutOfBounds(const OutOfBounds&) throw() {}
+				OutOfBounds &operator=(const OutOfBounds&) throw() {}
+				const char *what() const throw() {return "Input is out of bounds.";}
+		} ;
+	private:
+		unsigned int _size;
+		T *_array;
+} ;
+
+#endif
--- module07/ex02/main.cpp
+++ module07/ex02/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        ::::::::            */
 /*   main.cpp                                           :+:    :+:            */
 /*                                                     +:+                    */
-/*   By: sfeith <sfeith@student.codam.nl>             +#+                     */
+/*   By: SophieLouiseFeith <SophieLouiseFeith@st      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/02/26 15:23:44 by sfeith        #+#    #+#                 */
-/*   Updated: 2021/02/26 15:27:06 by sfeith        ########   odam.nl         */
+/*   Updated: 2021/03/02 12:58:02 by SophieLouis   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,4 +28,40 @@ int main(void){
 
 
     return 0;
+}
+
+
+#include "Array.hpp"
+
+int main(){
+	Array<int> test1(3);
+	Array<float> test2(5);
+	Array<char> test3(7);
+	try {
+		for (unsigned int i = 0; i < test1.size(); i++)
+			std::cout << test1[i] << " ";
+		std::cout << std::endl ;
+		std::cout << "-- Trying to access an element that is out of bounds. --" << std::endl ;
+		test1[5];
+	}
+	catch (std::exception &e){
+		std::cout << e.what() << std::endl ;
+	}
+	try{
+		for (unsigned int i = 0; i < test2.size(); i++){
+			test2[i] = i + 0.1 ;
+			std::cout << test2[i] << " ";
+		}
+		std::cout << std::endl ;
+		for (unsigned int i = 0; i < test3.size(); i++){
+			test3[i] = 97 + i;
+			std::cout << test3[i] << " ";
+		}
+		std::cout << std::endl ; 
+		std::cout << test3.size() << std::endl ;
+	}
+	catch(std::exception &e){
+		std::cout << e.what() << std::endl;
+	}
+	return (0);
 }
